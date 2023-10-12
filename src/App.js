9e98acf123c8f12891c3377cb0c74427957d05d1
manish6265 <127199419+manish6265@users.noreply.github.com>
2023-10-12 16:42:41 +0530

    Update App.js
    add alert

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     {/*<Alert alert={alert}/>*/}
       <div className="container my-3">
-        {/*<About></About>*/}
+        <About></About>
         {/*<TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert}/>*/}
         {<Router>
           <Routes>
